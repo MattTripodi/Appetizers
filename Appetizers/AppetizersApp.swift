2bc23b5043c750cddfece1ca8a253e1cfd661352
MattTripodi <mtripodi1027@gmail.com>
2025-03-17 20:55:22 -0400

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Matthew Tripodi on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
